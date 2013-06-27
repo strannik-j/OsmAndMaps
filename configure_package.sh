79c25948d9b8dbda7e3b2c8f361d2a97a0ca1c39
Strannik-j <mail@strannik-j.org>
2013-06-27 22:26:07 +0400

    fix

--- configure_package.sh
+++ configure_package.sh
@@ -3,12 +3,12 @@
 package=$1
 package_dir=$(echo src/$package|sed 's/\./\//g')
 mkdir -p $package_dir
-mv src/com/dummy/fooforandroid/* $package_dir/
-rmdir src/com/dummy/fooforandroid
-rmdir src/com/dummy
+mv src/com/strannikj/osmandmaps/* $package_dir/
+rmdir src/com/strannikj/osmandmaps
+rmdir src/com/strannikj
 rmdir src/com
 source_files=$package_dir/*
 for filename in $source_files AndroidManifest.xml build.properties;
 do
-	sed 's/com\.dummy\.fooforandroid/'$package'/g' $filename > tmp; mv tmp $filename;
+	sed 's/com\.strannikj\.osmandmaps/'$package'/g' $filename > tmp; mv tmp $filename;
 done
